0e8988797e5e144b5cd97f31268c68f437b551f0
MakKir <kmmakarov@mail.ru>
2020-05-30 09:11:18 +0300

    finished basic framework functionality (#3)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import {Excel} from '@/components/excel/excel'
 import './scss/index.scss'
+import {Toolbar} from '@/components/toolbar/toolbar'
+import {Header} from '@/components/header/Header'
+import {Formula} from '@/components/formula/formula'
+import {Table} from '@/components/table/Table'
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
